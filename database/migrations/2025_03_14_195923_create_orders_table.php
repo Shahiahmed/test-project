ea6ad4ddc380bc912321e8bbd9bb05ce76c97153
Ahmet Askar <87872732+Shahiahmed@users.noreply.github.com>
2025-03-15 01:01:59 +0500

    migrations added

---
+++ database/migrations/2025_03_14_195923_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name'); // ФИО покупателя
+            $table->date('order_date'); // дата создания
+            $table->enum('status', ['новый', 'выполнен'])->default('новый');
+            $table->text('customer_comment')->nullable();
+            $table->unsignedBigInteger('product_id'); // связь с товаром
+            $table->integer('quantity')->default(1);
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
